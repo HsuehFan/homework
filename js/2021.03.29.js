8177fee12f59fe2c6e2bc31b7eed5b8f22fc6dfc
RitaHuang <01236look@gmail.com>
2021-03-29 15:09:45 +0800

    2021年3月29日每日任務

---
+++ js/2021.03.29.js
@@ -0,0 +1,7 @@
+let pb1 = document.querySelector('.pb1');
+let pb2 = document.querySelector('.pb2');
+let pb3 = document.querySelector('.pb3');
+
+pb1.textContent = `屬於 applicaiton/x-www-form-urlcoded 類型。`;
+pb2.textContent = `屬於 multipart/form-data 類型`;
+pb3.textContent = `屬於 applicaiton/json 類型`;
