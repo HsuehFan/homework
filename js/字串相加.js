feccee4fa3b69e9a8ce8ba32529b67cdade73707
RitaHuang <01236look@gmail.com>
2021-03-29 14:45:30 +0800

    Update 字串相加.js

--- js/字串相加.js
+++ js/字串相加.js
@@ -1,17 +1,31 @@
 // 範例一：字串相加
-const title = document.querySelector("h3");
+
+let title = document.querySelector('h3');
 let str = `<span style="color:red">紅色標題</span>`;
-str+=`<span style="color:blue">藍色標題</span>`;
+str += `<span style="color:blue">藍色標題</span>`;
 title.innerHTML = str;
 
-
-const list = document.querySelector(".list");
-let data = [
-  "hello","你好嗎？"
-];
-
+let list = document.querySelector('.list');
+let data = ["Hello","早安～♥"];
 let str2 = "";
 data.forEach(function(item){
-  str2+=`<li>${item}</li>`
+    str2 += `<li>${item}</li>` 
 })
 list.innerHTML = str2;
+
+// const title = document.querySelector("h3");
+// let str = `<span style="color:red">紅色標題</span>`;
+// str+=`<span style="color:blue">藍色標題</span>`;
+// title.innerHTML = str;
+
+
+// const list = document.querySelector(".list");
+// let data = [
+//   "hello","你好嗎？"
+// ];
+
+// let str2 = "";
+// data.forEach(function(item){
+//   str2+=`<li>${item}</li>`
+// })
+// list.innerHTML = str2;
