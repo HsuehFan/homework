1478170ea34adb04b023c3b00f856f26afef9226
RitaHuang <01236look@gmail.com>
2021-03-29 23:29:14 +0800

    第五週作業LV1完成

--- js/five_week.js
+++ js/five_week.js
@@ -31,9 +31,38 @@ let data = [
     }
   ];
 
-let test1 = "";
-const ticketCardName = document.querySelector('.ticketCard-name');
+let str = "";
+const ticketCardName = document.querySelector('.ticketCard-area');
 data.forEach(function(item){
-    test1 = item.name;
-    console.log(test1);
+    str += `
+    <li class="ticketCard">
+        <div class="ticketCard-img">
+            <a href="#">
+                <img src="${item.imgUrl}" alt="">
+            </a>
+            <div class="ticketCard-region">${item.area}</div>
+            <div class="ticketCard-rank">${item.rate}</div>   
+        </div>
+        <div class="ticketCard-content">
+            <div>
+                <h3>
+                    <a href="#" class="ticketCard-name">${item.name}</a>
+                </h3>
+                <p class="ticketCard-description">
+                    ${item.description}
+                </p>
+            </div>
+            <div class="ticketCard-info">
+                <p class="ticketCard-num">
+                    <span><i class="fas fa-exclamation-circle"></i></span>
+                    剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
+                </p>
+                <p class="ticketCard-price">
+                    TWD <span id="ticketCard-price">${item.price}</span>
+                </p>
+            </div>    
+        </div>
+    </li>
+    `;
 })
+ticketCardName.innerHTML = str;
