30c92fecb876baa2b4ab33f15e414823b9c14db6
RitaHuang <01236look@gmail.com>
2021-04-01 15:25:02 +0800

    index修改1

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const urlpath = "https://hsuehfan.github.io/homework/directoryData.json";
+const urlpath2 = "";
+let site1 = document.querySelector('.site1');
+let site2 = document.querySelector('.site2');
+let datalist = "";
+let urlManager = "";
+axios.get(urlpath)
+.then(function(response){
+    datalist = response.data;
+    // console.log(datalist)
+    datalist.forEach(function(item){
+        
+        urlManager += `
+        <a href="${item.urlLocalhost}">${item.titleName}</a></br>   
+        `;
+    })
+    site1.innerHTML = `${urlManager}`;
+});
