95140b42be8c918fe4b011fe33ecaf81bb68c86d
RitaHuang <01236look@gmail.com>
2021-04-01 21:27:56 +0800

    2021.04.01每日任務

---
+++ js/2021.04.01.js
@@ -0,0 +1,28 @@
+let answer = {};
+let urlPath = "https://raw.githubusercontent.com/hexschool/js-traninging-week6API/main/data.json";
+let answer1 = document.querySelector('.answer1');
+axios.get(urlPath)
+    .then((response) =>{
+        let total = {};
+        let data = response.data;
+
+        data.forEach((item, i) =>{
+            if(total[item.jsGroup]===undefined){
+                total[item.jsGroup] = 1;
+            } else {
+                total[item.jsGroup] +=1;
+            }
+            
+        })
+        let dataProcess = Object.keys(total);
+            dataProcess.forEach((item)=>{
+                if(item !=="未分組"){
+                    answer[`第 ${item} 組人數`] = total[item];
+
+                }else{
+                    answer[`第 ${item} 人數`] = total[item];
+
+                }
+            })
+            console.log(answer);
+    })
