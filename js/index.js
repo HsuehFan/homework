c95515d4f547891a3932c7202db8e258c8e4db28
RitaHuang <01236look@gmail.com>
2021-04-07 16:39:00 +0800

    目錄2測試

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const urlpath = "https://hsuehfan.github.io/homework/directoryData.json";
-const urlpath2 = "";
+const urlpath2 = "https://hsuehfan.github.io/homework/directoryData2.json";
 let site1 = document.querySelector('.site1');
 let site2 = document.querySelector('.site2');
 let datalist = "";
@@ -16,4 +16,17 @@ axios.get(urlpath)
         `;
     })
     site1.innerHTML = `${urlManager}`;
+});
+
+axios.get(urlpath2)
+.then(function(response){
+    datalist = response.data;
+    // console.log(datalist)
+    datalist.forEach(function(item){
+        
+        urlManager += `
+        <a href="${item.urlLocalhost}">${item.titleName}</a></br>   
+        `;
+    })
+    site2.innerHTML = `${urlManager}`;
 });
