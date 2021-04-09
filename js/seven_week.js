96e9abfc9760e11b2c1988fae1f636eb1e138e97
RitaHuang <01236look@gmail.com>
2021-04-09 22:33:48 +0800

    第七週主線任務

--- js/seven_week.js
+++ js/seven_week.js
@@ -1,57 +1,36 @@
-let people = [
-    {
-        area:"高雄",
-        name:"小明"
-    },
-    {
-        area: "高雄",
-        name: "小天"
-    },
-    {
-        area:"台中",
-        name:"小華"
-    },
-    {
-        area: "台北",
-        name: "小華"
-    }
-]
+const urlPath = "https://raw.githubusercontent.com/hexschool/js-training/main/travelAPI-lv1.json";
 
-  // 篩選地區，並累加數字上去
-  // totalObj 會變成 {高雄: 2, 台北: 1, 台中: 2}
 let totalObj = {};
-    people.forEach(function(item){
-        if(totalObj[item.area] == undefined){
+axios.get(urlPath)
+    .then(function(res){
+        let data = res.data;
+    data.forEach(function(item){
+        if(totalObj[item.area] === undefined){
             totalObj[item.area] = 1;
-        }else{
+        } else {
             totalObj[item.area] += 1;
         }
     })
-    console.log(totalObj);
 
-  // newData = [["高雄", 2], ["台北",1], ["台中", 1]]
-let newData = [];
-let area = Object.keys(totalObj);
-console.log(area);
-  // area output ["高雄","台北","台中"]
-area.forEach(function(item,index){
-    let ary = [];
-    console.log(item);
-    ary.push(item);
-    console.log(totalObj[item]);
-    ary.push(totalObj[item]);
+    newData = [];
+    let area = Object.keys(totalObj);
+    // area output ["高雄","台北","台中"]
+    area.forEach(function(item){
+        let arr = [];
+        arr.push(item);
+        arr.push(totalObj[item]);
+        newData.push(arr);
+    })
 
-    newData.push(ary);
+      // 將 newData 丟入 c3 產生器
+    const chart = c3.generate({
+        bindto: ".chart",
+        data: {
+            columns: newData,
+            type: 'donut'
+        },
+        donut: {
+            title: "地區"
+        }
+    })
 })
-console.log(newData);
-  // 將 newData 丟入 c3 產生器
-const chart = c3.generate({
-    bindto: "#chart",
-    data: {
-        columns: newData,
-        type : 'donut',
-    },
-    donut: {
-        title: "地區"
-    }
-});
